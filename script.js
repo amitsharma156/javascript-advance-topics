0d8ccbe9646dbdc1bc5eea12f99103ede3e46589
Amit <sharma88.amit@gmail.com>
2019-12-03 01:15:57 +0530

    javascript advance revision primitives vs objects

--- script.js
+++ script.js
@@ -39,20 +39,53 @@
 
 // constructor using object.create
 
-var personProto ={
-    calculateAge : function(){
-        console.log(2016 - this.yearOFBrith);
-    }
-};
+// var personProto ={
+//     calculateAge : function(){
+//         console.log(2016 - this.yearOFBrith);
+//     }
+// };
 
-var john = Object.create(personProto);
-john.name = 'John';
-john.yearOFBrith = 1990,
-john.job = 'teacher';
+// var john = Object.create(personProto);
+// john.name = 'John';
+// john.yearOFBrith = 1990,
+// john.job = 'teacher';
 
-var jane = Object.create(personProto,{
-    name : {value:'jane'},
-    yearOFBrith : {value : 1969},
-    job : {value : 'designer'}
-});
+// var jane = Object.create(personProto,{
+//     name : {value:'jane'},
+//     yearOFBrith : {value : 1969},
+//     job : {value : 'designer'}
+// });
 
+//Primitives vs objects
+
+//primitives
+
+var a =23 ;
+var b = a ;
+a =46;
+console.log(a,b);
+
+//Objects
+var obj1 ={
+    name :'John',
+    age :26
+}
+var obj2 = obj1;
+obj1.age =30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+//functions
+
+var age = 27;
+var obj = {
+    name:'jonas',
+    city :'Lisbon'
+}
+function change(a,b){
+    a=30;
+    b.city ='san'
+}
+change(age,obj);
+console.log(age);
+console.log(obj.city);
