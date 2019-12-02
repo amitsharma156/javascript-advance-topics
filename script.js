804fe1c929895570bb51338432159b78bca6e1ae
Amit <sharma88.amit@gmail.com>
2019-12-02 13:29:24 +0530

    Javascript Advanced passing function as arguments

--- script.js
+++ script.js
@@ -46,33 +46,66 @@
 
 // Primitives vs Objects
 
-var a = 23;
-var b = a;
-a=46;
-console.log(a, b);
+// var a = 23;
+// var b = a;
+// a=46;
+// console.log(a, b);
 
-var obj1 ={
-    name:'john',
-    age : 26
+// var obj1 ={
+//     name:'john',
+//     age : 26
+// }
+// var obj2 = obj1;
+// obj1.age =30;
+// console.log(obj1);
+// console.log(obj2);
+
+// //functions 
+// var age = 27;
+// var obj = {
+//     name : 'jonas',
+//     city : 'Lisbon'
+// };
+// function change(a,b){
+//     a=30;
+//     b.city ='San Fransicco';
+// }
+
+// change(age,obj);
+
+// console.log(age);
+// console.log(obj.city);
+
+//Passing functions as arguments
+
+var years = [1990,1965,1972,1928,1999];
+
+function arrayCalc(arr,fn){
+    var newArr = [];
+    for( var i=0; i<arr.length ; i++){
+        newArr.push(fn(arr[i]));
+    }
+    return newArr;
 }
-var obj2 = obj1;
-obj1.age =30;
-console.log(obj1);
-console.log(obj2);
 
-//functions 
-var age = 27;
-var obj = {
-    name : 'jonas',
-    city : 'Lisbon'
-};
-function change(a,b){
-    a=30;
-    b.city ='San Fransicco';
+function calculateAge(age){
+    return 2010-age;
+}
+function adult(age){
+    return age>=18;
+}
+function maxHeartRate(age){
+    if(age>=18 && age<81){
+        return 206-age;
+    }
+    else{
+        return -1;
+    }
 }
 
-change(age,obj);
-
-console.log(age);
-console.log(age.city);
-
+var ages = arrayCalc(years,calculateAge);
+var adullt =arrayCalc(ages,adult)
+var heart = arrayCalc(ages,maxHeartRate);
+console.log(ages);
+console.log(adullt);
+console.log(heart);
